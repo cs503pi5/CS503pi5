54810163f668bf877d74e8a272dd2c4150145ca1
Vincent Tran <vincenttran7411@gmail.com>
2018-11-08 17:29:47 -0500

    update main pi and main ard to count both wheels and display

---
+++ Main/motor.ino
@@ -0,0 +1,95 @@
+#include "DualMC33926MotorShield.h"
+
+DualMC33926MotorShield md;
+
+void stopIfFault()
+{
+  if (md.getFault())
+  {
+    while(1){
+        Serial.println("fault");
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Dual MC33926 Motor Shield");
+  md.init();
+}
+
+void loop()
+{
+  for (int i = 0; i <= 400; i++)
+  {
+    md.setM1Speed(i);
+    stopIfFault();
+    if (abs(i)%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = 400; i >= -400; i--)
+  {
+    md.setM1Speed(i);
+    stopIfFault();
+    if (abs(i)%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = -400; i <= 0; i++)
+  {
+    md.setM1Speed(i);
+    stopIfFault();
+    if (abs(i)%200 == 100)
+    {
+      Serial.print("M1 current: ");
+      Serial.println(md.getM1CurrentMilliamps());
+    }
+    delay(2);
+  }
+
+  for (int i = 0; i <= 400; i++)
+  {
+    md.setM2Speed(i);
+    stopIfFault();
+    if (abs(i)%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = 400; i >= -400; i--)
+  {
+    md.setM2Speed(i);
+    stopIfFault();
+    if (abs(i)%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+  
+  for (int i = -400; i <= 0; i++)
+  {
+    md.setM2Speed(i);
+    stopIfFault();
+    if (abs(i)%200 == 100)
+    {
+      Serial.print("M2 current: ");
+      Serial.println(md.getM2CurrentMilliamps());
+    }
+    delay(2);
+  }
+}
