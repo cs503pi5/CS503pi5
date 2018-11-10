b99ef3f8fc9ba126cc77cbb59602eee6d5be65d8
Vincent Tran <vincenttran7411@gmail.com>
2018-11-10 15:22:51 -0500

    move the timing value of arduino

--- pwm_vs_vel/arduino.ino
+++ pwm_vs_vel/arduino.ino
@@ -9,16 +9,11 @@ DualMC33926MotorShield md;
 #define outputR1 3
 #define outputR2 2
 
-int left_counter = 0; 
-int right_counter = 0; 
-
-
 int lState;
 int lLastState;  
 
 int rState;
 int rLastState;  
-long start_millis;
 
 void stopIfFault()
 {
@@ -48,9 +43,12 @@ void setup() {
 void loop() { 
   for (int pwm_count = 80; pwm_count < 100; pwm_count = pwm_count + 5){
     //inital sets
-    start_millis = millis();
+    long start_millis = millis();
     md.setM1Speed(pwm_count);
     md.setM2Speed(pwm_count);
+    int left_counter = 0; 
+    int right_counter = 0; 
+
     stopIfFault();
     while(left_counter + right_counter < 100){
       lState = digitalRead(outputL1);
@@ -96,8 +94,5 @@ void loop() {
     Serial.println("Right wheel centimeters per second: " + String(r_cps));
     Serial.println("Light wheel centimeters per second: " + String(l_cps));
 
-  //clear for next loop
-    right_counter = 0;
-    left_counter = 0; 
   }
 }
