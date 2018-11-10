87db3fb48111e12c1a4c218d4964ad1901884e3d
Vincent Tran <vincenttran7411@gmail.com>
2018-11-10 15:17:47 -0500

    connect python and arduino

--- pwm_vs_vel/arduino.ino
+++ pwm_vs_vel/arduino.ino
@@ -1,67 +1,103 @@
+#include "DualMC33926MotorShield.h"
 
- #define outputL1 6
- #define outputL2 5
+DualMC33926MotorShield md;
+
+#define outputL1 6
+#define outputL2 5
 
 
- #define outputR1 3
- #define outputR2 2
+#define outputR1 3
+#define outputR2 2
+
+int left_counter = 0; 
+int right_counter = 0; 
+
+
+int lState;
+int lLastState;  
+
+int rState;
+int rLastState;  
+long start_millis;
+
+void stopIfFault()
+{
+  if (md.getFault())
+  {
+    Serial.println("fault");
+    while(1);
+  }
+}
+
+
+void setup() { 
+  pinMode (outputL1,INPUT);
+  pinMode (outputL2,INPUT);
+
+  pinMode (outputR1,INPUT);
+  pinMode (outputR2,INPUT);
  
- int left_counter = 0; 
- int right_counter = 0; 
+  Serial.begin (9600);
+  // Serial.println("starting..");
 
- 
- int lState;
- int lLastState;  
+  lLastState = digitalRead(outputL1);   
+  rLastState = digitalRead(outputR1);  
+  md.init();
 
- int rState;
- int rLastState;  
- long start_millis;
-
- void setup() { 
-   pinMode (outputL1,INPUT);
-   pinMode (outputL2,INPUT);
-
-   pinMode (outputR1,INPUT);
-   pinMode (outputR2,INPUT);
-   
-   Serial.begin (9600);
-   Serial.println("starting..");
-
-  start_millis = millis();
-   lLastState = digitalRead(outputL1);   
-   rLastState = digitalRead(outputR1);  
- } 
- void loop() { 
-  
-  while(left_counter + right_counter < 100){
-    lState = digitalRead(outputL1);
-     
-    if (lState != lLastState){     
-      if (digitalRead(outputL2) != lState) { 
-        left_counter ++;
-      } else {
-        left_counter --;
-      }
-      // Serial.print("Position: " + String(((float)left_counter/20L)));
-    } 
-    lLastState = lState;
-  
-    rState = digitalRead(outputR1);
-     
-    if (rState != rLastState){     
-      if (digitalRead(outputR2) != rState) { 
-        right_counter ++;
+} 
+void loop() { 
+  for (int pwm_count = 80; pwm_count < 100; pwm_count = pwm_count + 5){
+    //inital sets
+    start_millis = millis();
+    md.setM1Speed(pwm_count);
+    md.setM2Speed(pwm_count);
+    stopIfFault();
+    while(left_counter + right_counter < 100){
+      lState = digitalRead(outputL1);
+       
+      if (lState != lLastState){     
+        if (digitalRead(outputL2) != lState) { 
+          left_counter ++;
         } else {
-        right_counter --;
+          left_counter --;
         }
-      // Serial.println("Position: " +  String(((float)right_counter/20L)));
+        // Serial.print("Position: " + String(((float)left_counter/20L)));
       } 
-    rLastState = rState;
-  } 
-  
-  long end_millis = millis();
-  double duration = (double)(end_millis - start_millis)/1000;
-  Serial.println(duration);
-  right_counter = 0;
-  left_counter = 0;
- }
+      lLastState = lState;
+    
+      rState = digitalRead(outputR1);
+       
+      if (rState != rLastState){     
+        if (digitalRead(outputR2) != rState) { 
+          right_counter ++;
+          } else {
+          right_counter --;
+          }
+        // Serial.println("Position: " +  String(((float)right_counter/20L)));
+        } 
+      rLastState = rState;
+    } 
+    
+    long end_millis = millis();
+    double duration = (double)(end_millis - start_millis)/1000;
+    // Serial.println(duration);
+    double circumference = 22.32914; //cm
+    double spoke_length = circumference / 20.0;
+
+    double r_cps = (spoke_length * right_counter) / duration;
+    double l_cps = (spoke_length * left_counter) / duration;
+    // Serial.println()
+    // Serial.println(spoke_length);
+    // Serial.println(left_counter);
+    // Serial.println(right_counter);
+
+    // Serial.println(duration);
+    Serial.println("For pwm = " + String(pwm_count));
+    Serial.println("Right wheel centimeters per second: " + String(r_cps));
+    Serial.println("Light wheel centimeters per second: " + String(l_cps));
+
+  //clear for next loop
+    right_counter = 0;
+    left_counter = 0; 
+  }
+}
