ef893f19e008b22fc84416950982b5c743bdd99f
Gandalf <cs503pi5@gmail.com>
2018-11-15 17:15:57 -0500

    hard code logic

--- main/main.py
+++ main/main.py
@@ -17,6 +17,8 @@ rawCapture = PiRGBArray(camera)
 
 theta_prev = 0
 
+COUNT_ = 30
+
 ## COMMUNICATION METHODS
 def python_write_string(infoToWrite):
     string_encode = infoToWrite.encode()
@@ -43,8 +45,15 @@ def interpret_odom(odometry):
 # right wheel cps = 0.162 * pwm - 19
 
 # get pwm for velocity
+# def get_l_pwm(v_cps):
+# 	return int( (v_cps + 18.7)/0.184)
+
+# # get pwm for velocity
+# def get_r_pwm(v_cps):
+# 	return int( (v_cps + 19)/0.162)
+
 def get_l_pwm(v_cps):
-	return int( (v_cps + 18.7)/0.184)
+	return int( (v_cps + 18.7)/0.166)
 
 # get pwm for velocity
 def get_r_pwm(v_cps):
@@ -56,7 +65,7 @@ def desired_velocity(c_ratio, v_ref):
 	v_left = (2*v_ref)/(c_ratio + 1)
 	return v_right, v_left
 
-def PD_error(theta_act, theta_ref, K = 0.5, B = 0.01):
+def PD_error(theta_act, theta_ref, K = 1, B = 0.01):
     global theta_prev
     theta_ddot = -K*(theta_act - theta_ref) - B*(theta_act-theta_prev)
     theta_prev = theta_act
@@ -86,17 +95,6 @@ def init():
     ser.flushInput()
     curr_odom = []
 
-def read_odom():
-    while (1):
-        ser.write(str(0)+','+str(0)+'\n'.encode())
-
-        message = python_read_line()
-        if message!=None:
-            curr_odom = interpret_odom(message)
-            print(curr_odom)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 def stop():
     i = 0
     while (i < 10):
@@ -142,27 +140,33 @@ def right_turn():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-def left_turn():
+def left_turn(curr_odom_):
     
-    curr_odom = [0,0,0]
+    curr_odom = curr_odom_
+    count = 0
+    while (curr_odom[2] < math.pi/2-.13):
+        if (count % (COUNT_) == 0):  
+            C = 1./80.
+            velocity_ref = 4.
+            desired_l, desired_r = desired_velocity(C, velocity_ref)
+            l_pwm = get_l_pwm(desired_l) + 29
+            r_pwm = get_r_pwm(desired_r) + 29
 
-    while (curr_odom[2] < math.pi/2):
-        C = 1./5.
-        velocity_ref = 7.
-        desired_l, desired_r = desired_velocity(C, velocity_ref)
-        l_pwm = get_l_pwm(desired_l) + 29
-        r_pwm = get_r_pwm(desired_r) + 29
+            s = str(l_pwm)+','+str(r_pwm)+'\n'.encode()
+            print(s)
+            print(curr_odom)
+            print time.asctime( time.localtime(time.time()) )
+            ser.write(s)
 
-        s = str(l_pwm)+','+str(r_pwm)+'\n'.encode()
-        print(s)
-        ser.write(s)
-
-        message = python_read_line()
-        if message!=None:
-            if (len(message) > 15):
-                curr_odom = interpret_odom(message)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            message = python_read_line()
+            if message!=None:
+                if (len(message) > 15):
+                    curr_odom = interpret_odom(message)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        count = count+1
+    return curr_odom
+    
 
 def first_fix():
     camera.capture(rawCapture, format="bgr")
@@ -221,86 +225,87 @@ def first_fix():
     rawCapture.truncate(0)
 
 
-def run_straight():
+def run_straight_y(distance,curr_odom_):
     pd_error = 0
-    curr_odom = [0,0,0]
+    curr_odom = curr_odom_
     count = 0
-    while (1):
-        # camera.capture(rawCapture, format="bgr")
-        # rawCapture.truncate(0)
 
-        # get the the last line, since pi faster than ard, guarantee to reach end of buffer
+    while (curr_odom[0] < distance):
         message = python_read_line()
-        #if there are still bytes, then consume
-        while(serial.in_waiting() > 0):
-            message = python_read_line()
         if message!=None:
             if (len(message) > 15):
                 curr_odom = interpret_odom(message)
                 pd_error = PD_error(curr_odom[2], 0)
 
-        # print(pd_error)
-        C = 1
-        velocity_ref = 5
-        desired_l, desired_r = desired_velocity(C, velocity_ref)
-        l_pwm = get_l_pwm(desired_l)
-        r_pwm = get_r_pwm(desired_r)
+        if (count % (COUNT_) == 0):        
+            C = 1
+            velocity_ref = 5
+            desired_l, desired_r = desired_velocity(C, velocity_ref)
+            l_pwm = get_l_pwm(desired_l)
+            r_pwm = get_r_pwm(desired_r)
 
-        l_pwm = l_pwm - int(pd_error)
-        r_pwm = r_pwm + int(pd_error)
+            l_pwm = l_pwm - int(pd_error)
+            r_pwm = r_pwm + int(pd_error)
 
-
-        s = str(l_pwm)+','+str(r_pwm)+'\n'.encode()
-        print(s)
-        print(curr_odom)
-        print time.asctime( time.localtime(time.time()) )
-        ser.write(s)
-
-        time.sleep(0.05)
+            l_pwm = 148
+            r_pwm = 148
+            s = str(l_pwm)+','+str(r_pwm)+'\n'.encode()
+            print(s)
+            print(curr_odom)
+            print time.asctime( time.localtime(time.time()) )
+            ser.write(s)
+        count = count + 1
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+    return curr_odom
 
+def run_straight_x(distance,curr_odom_):
+    count = 0
+    message = python_read_line()
+    curr_odom = curr_odom_
+    if message!=None:
+        if (len(message) > 15):
+            curr_odom = interpret_odom(message)
 
-def long_straight():
-    curr_odom = [0,0,0]
-    # message = python_read_line()
-    # if message!=None:
-    #     if (len(message) > 15):
-    #         curr_odom = interpret_odom(message)
-
-
-    while (curr_odom[0] < 100):
-
-        # camera.capture(rawCapture, format="bgr")
-        # rawCapture.truncate(0)
-
+    goal = curr_odom[0] + distance
+    while (curr_odom[0] < goal):
         message = python_read_line()
         if message!=None:
             if (len(message) > 15):
                 curr_odom = interpret_odom(message)
 
-        # print(pd_error)
-        C = 1
-        velocity_ref = 6
-        desired_l, desired_r = desired_velocity(C, velocity_ref)
-        l_pwm = get_l_pwm(desired_l)
-        r_pwm = get_r_pwm(desired_r)
+        if (count % (COUNT_) == 0):        
+            C = 1
+            velocity_ref = 5
+            desired_l, desired_r = desired_velocity(C, velocity_ref)
+            l_pwm = get_l_pwm(desired_l)
+            r_pwm = get_r_pwm(desired_r)
 
+            l_pwm = 148
+            r_pwm = 148
+            s = str(l_pwm)+','+str(r_pwm)+'\n'.encode()
+            print(s)
+            print(curr_odom)
+            print time.asctime( time.localtime(time.time()) )
+            ser.write(s)
+        count = count + 1
 
-        s = str(l_pwm)+','+str(r_pwm)+'\n'.encode()
-        print(curr_odom)
-        # print(l_pwm, r_pwm)
-        serial.write(s)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+    return curr_odom
 if __name__ == "__main__":
     print("Initializing...")
-    init() # just flushes and for some reason creates a curr_odom object in its scope
-    print("Running...")
-    run_straight()
+    init() 
+    curr_odom = [0,0,0]
+    print("Running straight...")
+    curr_odom = run_straight_x(30,curr_odom)
+    print("Left turn...")
+    curr_odom = left_turn(curr_odom)
+    print("Running straight...")
+    curr_odom = run_straight_y(80,curr_odom)
     print("Stopping...")
     stop()
+    print(curr_odom)
