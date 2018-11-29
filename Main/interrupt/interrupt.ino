bbb576ad02fcba324ac275c9c015154562aa88cf
Gandalf <cs503pi5@gmail.com>
2018-11-29 15:13:18 -0500

    changes

--- Main/interrupt/interrupt.ino
+++ Main/interrupt/interrupt.ino
@@ -1,5 +1,9 @@
+#include "DualMC33926MotorShield.h"
+
+DualMC33926MotorShield md;
 
 volatile long right_enc_count = 0;
+
 volatile long left_enc_count = 0;
 
 // goes digital pins go 0 1 2 3 4 5 6 7
@@ -27,7 +31,7 @@ void setup() {
   pinMode(leftOutputB, INPUT);
 
   Serial.begin(115200);
-  Serial.println("0,0");
+  //Serial.println("0,0");
 }
 
 void loop() {
@@ -48,7 +52,6 @@ void loop() {
             int start = inData.indexOf(',');
             int end_pt = inData.indexOf('\n');
             int rpwm = inData.substring(start+1, end_pt).toInt();
-
             set_lwheel(lpwm);
             set_rwheel(rpwm);
 
@@ -64,14 +67,35 @@ void loop() {
     // update_cord();
 
     // String cord_vals = String(x_cord) + "," + String(y_cord) + "," + String(theta);
-    Serial.println(String(left_enc_count) + ","+String(right_enc_count));
+ //   Serial.println(String(left_enc_count) + ","+String(right_enc_count));
     }
+//}
+// motors
+void stopIfFault(){
+  if (md.getFault())
+  {
+    while(1){
+        Serial.println("fault");
+    }
+  }
+}
+void set_lwheel(int speed){
+  md.setM2Speed(speed);
+  stopIfFault();
+}
+
+// rwheel m1
+void set_rwheel(int speed){
+  md.setM1Speed(speed);
+  stopIfFault();
+
 }
 
 void left_encoder_isr() {
     static int8_t left_lookup_table[] = {0,0,0,-1,0,0,1,0,0,1,0,0,-1,0,0,0};
     static uint8_t left_enc_val = 0;
-    
+    //int countL = 0;
+    //countL++;
     left_enc_val = left_enc_val << 2;
     //PIND reads all pin inputs from pins 0 to 7, our pins of interest are pins 3 and 4 for the left wheel
     uint8_t temp = 0;
@@ -79,17 +103,22 @@ void left_encoder_isr() {
     temp = temp | (PIND >> 4 & 0b10);
     left_enc_val = left_enc_val | temp;
     left_enc_count = left_enc_count + left_lookup_table[left_enc_val & 0b1111];
-    // Serial.println(String(left_enc_count) + ","+String(right_enc_count));
+    //if(countL % 3 == 0){
+      Serial.println(String(left_enc_count) + ","+String(right_enc_count));
+    //}
 }
 void right_encoder_isr() {
     static int8_t lookup_table[] = {0,0,0,-1,0,0,1,0,0,1,0,0,-1,0,0,0};
     static uint8_t right_enc_val = 0;
-    
+    //int countR = 0;
+    //countR++;
     right_enc_val = right_enc_val << 2;
     //PIND reads all pin inputs from pins 0 to 7, our pins of interest are pins 3 and 4 for the right wheel
     right_enc_val = right_enc_val | ((PIND & 0b11000) >> 3);
     right_enc_count = right_enc_count + lookup_table[right_enc_val & 0b1111];
     // Serial.println("Right Counter: " + String(right_enc_count));
-    // Serial.println(String(left_enc_count) + ","+String(right_enc_count));
+    //if(countR % 3 == 0){
+     // Serial.println(String(left_enc_count) + ","+String(right_enc_count));
+   // }
+    Serial.println(String(left_enc_count) + ","+String(right_enc_count));
 }
-
