e41e261d10605ed6a950f8174035a8513c2e34b7
Vincent Tran <vincenttran7411@gmail.com>
2018-12-05 19:57:43 -0500

    turn left

--- Main/interrupt_connect.py
+++ Main/interrupt_connect.py
@@ -466,7 +466,7 @@ if __name__ == "__main__":
     time.sleep(4)
 
     ser.flushInput()
-    run_straight_x(50,0)
+    # run_straight_x(50,0)
     turn_left(-np.pi/2)
     # run_straight_y(119,0)
 #     run_straight_x_visual(50,0)
