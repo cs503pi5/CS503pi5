23b673afac42d8577c9809fe2284af0bee9ff63d
Vincent Tran <vincenttran7411@gmail.com>
2018-12-07 15:56:43 -0500

    py edits

--- Main/interrupt_connect_camera.py
+++ Main/interrupt_connect_camera.py
@@ -1,4 +1,4 @@
-bimport serial
+import serial
 import time
 import numpy as np
 import cv2
@@ -7,7 +7,7 @@ import time
 import sys
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-from camera import get_error
+# from camera import get_error
 
 port = '/dev/ttyACM0'
 ser = serial.Serial(port, 115200)
@@ -40,6 +40,10 @@ def time_wait(seconds):
 
 
 # returns a double derived theta which is similar to the velocity added to right wheel and removed from left wheel
+# when the car is pointed to the right, aka need a increase speed to R and decrease in L to fix
+# will see a positive error since midpoint - (a point < midpoint)
+# positive error * -K results in a negative cam_ddot
+# therefore we subtract neg from right and add negative to thet left
 def PD_error_camera(camera_error, camera_ref, K, B):
     global prev_error
     cam_ddot = -K*(camera_error - camera_ref) - B*(camera_error-prev_error)
@@ -72,13 +76,12 @@ def stop():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-			
-			
+
 def run_straight_x_visual(ref):
 
     # send inital velocities to arduino of wait to set the wheel pwms
     C = 1
-    velocity_ref = 4
+    velocity_ref = 5
     l_ref_velocity, r_ref_velocity = desired_velocity(C, velocity_ref)
     l_velocity = l_ref_velocity
     r_velocity = r_ref_velocity
@@ -105,36 +108,30 @@ def run_straight_x_visual(ref):
     time_wait(poll_time)
 
         # update change left and right wheel
-        #l_w_turns,r_w_turns = get_wheel_turns()
+        l_w_turns,r_w_turns = get_wheel_turns()
         #print(curr_odom)
 
-        #l_distance = get_distance(l_w_turns)
+        l_distance = get_distance(l_w_turns)
         # l_wheel_cps = ldistance / poll_time
 
-        #r_distance = get_distance(r_w_turns)
+        r_distance = get_distance(r_w_turns)
         # l_wheel_cps = r_distance / poll_time
 
-        #delta_left = l_distance
-        #delta_right = r_distance
+        delta_left = l_distance
+        delta_right = r_distance
 
         # update odometer x,y,theta
-        #update_cord(delta_left, delta_right)
+        update_cord(delta_left, delta_right)
 
         # get pd error from updated thetas
         # curr_theta = curr_odom[2]
+
+        # function from camera
         curr_visual_error = get_error()
-        approx_velocity = PD_error(curr_visual_error, ref, K=.5, B=0.1)
+        # since we're talking about pixels here the error could reasonably be [-30,30]
+        # approx_velocity = PD_error(curr_visual_error, ref, K=.5, B=0.1)
+        approx_velocity = PD_error_camera(curr_visual_error, camera_ref = 0, K=0.5, 0.1)
 
-        # # change velocity according to pd error
-        # if (r_velocity + approx_velocity) > get_r_cps(160) or (r_velocity + approx_velocity) < get_r_cps(120):
-
-        #     pass
-        # else:
-        #     r_velocity = r_velocity + approx_velocity
-        # if (l_velocity - approx_velocity) > get_l_cps(160) or (l_velocity + approx_velocity) < get_r_cps(120):
-        #     pass
-        # else:
-        #     l_velocity = l_velocity - approx_velocity
         r_velocity = r_velocity - approx_velocity
         l_velocity = l_velocity + approx_velocity
 
@@ -149,7 +146,8 @@ def run_straight_x_visual(ref):
 camera = PiCamera()
 rawCapture = PiRGBArray(camera)
 rawCapture.truncate(0)
-camera_midpoint = 310
+#640 width 480 height
+camera_midpoint = 320
 white_offset = 100
 tolerance = 40
 width = 400 #will change
@@ -166,22 +164,29 @@ def isWhite(array):
 def isRed(array):
     if (array[0] < 150 and array[1] < 150 and array[2] > 200):
         return True
-def get_error():
+
+def get_visual_error():
     global width
     red_seen = False
     camera.capture(rawCapture, format="bgr")
     image = rawCapture.array
+    # 345-275 = 70
     crop = image[275:345,0:640]
 
+    # init the yellow and white cord to -1,-1 
     yellow = [-1,-1]
     white = [-1,-1]
-    for y in range(69,0,-1):
-        for x in range(320,0,-1):
+    
+    # find the yellow pixel 
+    for y in range(69,0,-1): #for every row
+        for x in range(320,0,-1): # for every column
             if (isRed(crop[y,x])):
                 red_seen = True
             if (isYellow(crop[y,x])):
                 yellow = [y,x]
                 break
+
+    # search for th white pixel in same row of yellow. theoretical should always find one
     line = yellow[0]
     for x in range(320,640):
         if (isWhite(crop[line,x])):
@@ -190,19 +195,20 @@ def get_error():
 
     midpoint = (white[1] + yellow[1])/2
 
-
+    # if no yellow, pad white by width/2 to be the midponit 
     if (yellow[0] == -1 and yellow[1] == -1):
         midpoint = white[1] - (width/2)
+
 
     error = camera_midpoint - midpoint
 
     if (yellow[0] != -1):
         width = white[1] - yellow[1]
 
-	PD_error_camera(error, camera_ref = 0, K = 1, B = 0.1)
+	# PD_error_camera(error, camera_ref = 0, K = 1, B = 0.1)
 		
     if (abs(error) > 100):
-        print("IGNORE")
+        print("IGNORE, meaning camera found white in the left half  ")
     else:
         print(error)
 
