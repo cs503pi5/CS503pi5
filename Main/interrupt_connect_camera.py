509aa4fff2536f49ab3fcb4585b4c1a97002e3f0
Vincent Tran <vincenttran7411@gmail.com>
2018-12-10 18:15:29 -0500

    print the pixels off

--- Main/interrupt_connect_camera.py
+++ Main/interrupt_connect_camera.py
@@ -303,9 +303,9 @@ def run_straight_x_visual(goal, ref):
         update_curr_odom()
 
         visual_error = get_visual_error()
-
+        print('pixels off',visual_error)
         approx_velocity = PD_error_camera(visual_error, camera_ref=0, K=.01, B=0.001)
-
+        
         r_velocity = r_velocity - approx_velocity
         l_velocity = l_velocity + approx_velocity
 
