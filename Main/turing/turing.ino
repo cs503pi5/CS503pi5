4f17985df4f40456fb4285eada610aad304c60e9
bin yu <binyu@1x-nat-vl930-172-30-123-237.wireless.umass.edu>
2018-12-12 15:13:26 -0500

    delete

--- Main/turing/turing.ino
+++ Main/turing/turing.ino
@@ -115,7 +115,7 @@ void update_wheels()
   double duration = (double)(end_millis - start_millis)/1000;
   // duration_pad = 0.0;
   double circumference = 22.32914; //cm
-  double spoke_length = circumference / 16.0;
+  double spoke_length = circumference / 20.0;
   curr_right_wheel_cps = (spoke_length * right_spoke_counter) / duration;
   curr_left_wheel_cps = (spoke_length * left_spoke_counter) / duration;
   delta_right = spoke_length * right_spoke_counter;
@@ -125,29 +125,28 @@ void update_wheels()
   }
 }
 
-void turn(int degree, int lpwm ,int rpwm) //input degree
+void turn(int radius, int lpwm ,int rpwm) //import the degree of turning
 {
   double delta_left_t = delta_left; //initialize the distance of left wheel
   double delta_right_t = delta_right; //initialize the distance of right wheel
-  double wheeldistance = 19.05; //set the radius of turning
-  double left_wheel_r = 9.14; //set radius of left is 0 cm.
-  double right_wheel_r = 28.19; //set radius of right is 10 cm.
-  double turn_left_c = 2*3.14159*left_wheel_r*(degree/360);//the length of walking on the left wheel when turning
-  double turn_right_c = 2*3.14159*right_wheel_r*(degree/360);//the length of walking on the right wheel when turning
+  double turning_radius = 10; //set the radius of turning
+  double left_wheel_r = 0; //set radius of left is 0 cm.
+  double right_wheel_r = 10; //set radius of right is 10 cm.
+  double turn_left_c = 2*3.14159*left_wheel_r*(radius/360);//the length of walking on the left wheel when turning
+  double turn_right_c = 2*3.14159*right_wheel_r*(radius/360);//the length of walking on the right wheel when turning
   double lenght_l;
   double lenght_r;
   double expect_left_v = curr_left_wheel_cps; //set basic speed is the curr-left speed
   double expect_right_v;
   
-
-while(1)
+  while(1)
   {
     
     update_wheels(); //update the distance and parameters
     lenght_l = abs(delta_left-delta_left_t);
     lenght_r = abs(delta_right-delta_right_t);
     expect_right_v = expect_left_v * turn_right_c/turn_left_c;//expected speed on right wheel
-    if (expect_right_v-curr_right_wheel_cps < -1.0)//if expected speed is smaller than current speed, slow down the speed
+    if (expect_right_v-curr_right_wheel_cps < -1.0)//if expected speed is smaller than current speed, slow down the speed 
     {
       if(expect_right_v-curr_right_wheel_cps < -3.0)//if too small then decrease rpwm more
       {
@@ -158,7 +157,7 @@ while(1)
           rpwm -= 3;
           }
       }
-      else if (expect_right_v-curr_right_wheel_cps > 1.0)//if expected speed is greater than current speed, speed up the speed
+      else if (expect_right_v-curr_right_wheel_cps > 1.0)//if expected speed is greater than current speed, speed up the speed 
     {
       if(expect_right_v-curr_right_wheel_cps > 3.0)//if too large then increase rpwm more
       {
@@ -169,57 +168,26 @@ while(1)
           rpwm += 3;
           }
       }
-    if(lenght_l <= 2.0 &&lenght_r<=2.0)//if accuracy of turning satisfy, then break
+    if(lenght_l <= 2.0 &&lenght_r<=2.0)//if accuracy of turning satisfy, then break 
     {
       break;
       }
       set_lwheel(lpwm);
       set_rwheel(rpwm);
     }
-  
   }
  
 void setup(){
   Serial.begin(115200);
   while(!Serial);
   md.init();
-  pinMode (LeftoutputA,INPUT);
-  pinMode (LeftoutputB,INPUT);
-  pinMode (RightoutputA, INPUT);
-  pinMode (RightoutputB, INPUT);
-  leftLastState = digitalRead(LeftoutputA);
-  rightLastState = digitalRead(RightoutputA);
-}
-//updates current velocities of each wheel and updates distance of each wheel
-String inData;
-void loop() {
-  while (Serial.available() > 0)
-  {
-    char rec = Serial.read();
-    inData += rec;
-    // Process message when new line character is recieved
-    if (rec == '\n')
-    {
-      // Serial.print(inData);
-      // extract lpwm and rpwm from the string
-      int lpwm = inData.substring(0,inData.indexOf(',')).toInt();
-      int start = inData.indexOf(',');
-      int end_pt = inData.indexOf('\n');
-      int rpwm = inData.substring(start+1, end_pt).toInt();
-      set_lwheel(lpwm);
-      set_rwheel(rpwm);
-      inData = ""; // Clear recieved buffer
-      //break incase new stuff comes and we're just stuck here
-      break;
-    }
-  }
-  update_wheels();
-  update_cord();
-  turn(90, 130 ,140); //turning
-  
-  set_lwheel(0);//first, go straight
-  set_rwheel(0);
-  delay(5000);
+  set_lwheel(120);//first, go straight
+  set_rwheel(120);
+  turn(90, 120 ,120); //turning
   String cord_vals = String(x_cord) + "," + String(y_cord) + "," + String(theta);
+
+}
+void loop() {
+  // put your main code here, to run repeatedly:
 
 }
