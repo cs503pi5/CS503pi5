bf4ba6b3e41cab50203fa5f82954b9eccb5502ae
Gandalf <cs503pi5@gmail.com>
2018-12-13 14:23:32 -0500

    new find midpoint

--- Main/camera_functions.py
+++ Main/camera_functions.py
@@ -16,23 +16,19 @@ prev_error = 0.0
 
 def isYellow(array):
     if (array[0] < 200 and array[1] > 200 and array[2] > 200):
-	print("a")
         return True
 
 def isWhite(array):
     if (array[0] > 200 and array[1] > 200 and array[2] > 200):
-        print("b")
-	return True
+	    return True
 
 def isRed(array):
     if (array[0] < 130 and array[1] < 130 and array[2] > 190):
-        print("c")
-	return True
+	    return True
 
 def isBlack(array):
-    if (array[0] < 50 and array[1] < 50 and array[2] < 50):
-        print("d")        
-	return True
+    if (array[0] < 50 and array[1] < 50 and array[2] < 50):      
+	    return True
 
 
 def take_picture():
@@ -41,7 +37,6 @@ def take_picture():
     rawCapture.truncate(0)
     camera.capture(rawCapture, format="bgr")
     image = rawCapture.array
-    print("e")
     return(image)
 
 def crop_image_full_road(image):
@@ -49,7 +44,6 @@ def crop_image_full_road(image):
     crop_end = 125
     size_of_crop = crop_end-crop_start
     crop = image[crop_start:crop_end,0:640]
-    print("f")
     return crop
 
 def crop_image_for_stop(image):
@@ -57,7 +51,6 @@ def crop_image_for_stop(image):
     crop_end = 480
     size_of_crop = crop_end-crop_start
     crop = image[crop_start:crop_end,200:400]
-    print("g")
     return crop
 
 ## Return True if need to stop (red)
@@ -74,10 +67,8 @@ def at_stop_sign(crop):
             if (isRed(crop[y,x])):
                 seen_red = True
     if (seen_green):
-	print("h")
         return False
     else:
-        print("i")
         return seen_red
         
 def find_yellow(crop):
@@ -86,17 +77,14 @@ def find_yellow(crop):
         for x in range(320,0,-10): # for every column
             if (isYellow(crop[y,x])):
                 yellow = [y,x]
-		print("j")
                 return yellow
     if yellow == [-1,-1]:
         for y in range(len(crop)-1,0,-2): #for every row
             for x in range(320,640,10): # for every column
                 if (isYellow(crop[y,x])):
                     yellow = [y,x]
-		    print("k")
                     return yellow
     else:
-	print("l")
         return [-1,-1]
 
 def find_white_from_yellow(crop, yellow):
@@ -105,9 +93,7 @@ def find_white_from_yellow(crop, yellow):
     for x in range(320,640):
         if (isWhite(crop[line,x])):
             white = [line,x]
-	    print("m")
             return white
-    print("n")
     return white
 
 ## Call if don't find yellow
@@ -117,17 +103,14 @@ def find_white(crop):
         for x in range(320,0,-10): # for every column
             if (isWhite(crop[y,x])):
                 white = [y,x]
-		print("o")
                 return white
     if white == [-1,-1]:
         for y in range(len(crop)-1,0,-2): #for every row
             for x in range(320,640,10): # for every column
                 if (isWhite(crop[y,x])):
                     white = [y,x]
-		    print("p")
                     return white
     else:
-	print("q")
         return [-1,-1]
 
 def find_midpoint(crop):
@@ -145,18 +128,15 @@ def find_midpoint(crop):
     else:
         midpoint = 320
     print(midpoint)
-    print("r")
     return midpoint
 
 def calculate_error(midpoint):
-    print("s")
     return (camera_midpoint - midpoint)
 
 def PD_error_camera(camera_error, camera_ref, K, B):
     global prev_error
     cam_ddot = -K*(camera_error - camera_ref) - B*(camera_error-prev_error)
     prev_error = camera_error
-    print("t")
     return cam_ddot
 
 
