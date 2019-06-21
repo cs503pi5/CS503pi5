00edab6e40b2078fbac8010d719fdcffd768bec5
cs503pi5 <44101121+cs503pi5@users.noreply.github.com>
2019-06-21 15:55:03 -0400

    Update pd_control.py

--- Main/camera/pd_control.py
+++ Main/camera/pd_control.py
@@ -2,12 +2,13 @@ import math
 
 prev_error = 0
 
-def PD_error_camera(camera_error, K, B):
+def PD_error_camera(camera_error, camera_ref, K, B):
     global prev_error
-    cam_ddot = -K*(camera_error) - B*(camera_error-prev_error)
+    cam_ddot = -K*(camera_error - camera_ref) - B*(camera_error-prev_error)
     prev_error = camera_error
     return cam_ddot
 
 def PD_to_PWM(cam_ddot):
     conversion_factor = 5
     return int(cam_ddot * conversion_factor)
+
