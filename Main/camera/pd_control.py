3370d107525f9a0f00c51bfcdb4cc6c361972dd9
cs503pi5 <44101121+cs503pi5@users.noreply.github.com>
2019-06-21 14:30:11 -0400

    Update pd_control.py

--- Main/camera/pd_control.py
+++ Main/camera/pd_control.py
@@ -6,13 +6,7 @@ def PD_error_camera(camera_error, camera_ref, K, B):
     global prev_error
     cam_ddot = -K*(camera_error - camera_ref) - B*(camera_error-prev_error)
     prev_error = camera_error
-    #if (abs(cam_ddot) < 30):
     return cam_ddot
-    #elif (cam_ddot > 30):
-    #    return 40
-    #else:
-    #    return -40
-
 
 def PD_to_PWM(cam_ddot):
     conversion_factor = 5
